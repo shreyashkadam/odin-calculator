e22534fd59d59b8ccd9ad2c4a37e636669a9bb85
shreyashkadam <shreyash.kadam10@gmail.com>
2022-09-27 00:46:41 +0530

    Create basic calculator functions

---
+++ script.js
@@ -0,0 +1,26 @@
+function add(){
+    let sum = 0;
+    for (let i = 0; i < arguments.length; i++){
+        sum += arguments[i];
+    }
+
+    return sum;
+}
+
+function subtract(){
+    return arguments[0] - arguments[1];
+}
+
+function multiply(){
+    prod = 1;
+    for(let i = 0; i < arguments.length; i++){
+        prod = prod * arguments[i];
+    }
+
+    return prod;
+}
+
+function divide(){
+    return arguments[0] / arguments[1];
+}
+

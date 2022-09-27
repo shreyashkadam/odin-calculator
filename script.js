a67873eea600a7a3ae56d5987d6f0dc13190aa06
shreyashkadam <shreyash.kadam10@gmail.com>
2022-09-27 10:07:32 +0530

    Create basic html calculator with buttons

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-function add(){
+function add() {
     let sum = 0;
-    for (let i = 0; i < arguments.length; i++){
+    for (let i = 0; i < arguments.length; i++) {
         sum += arguments[i];
     }
 
     return sum;
 }
 
-function subtract(){
+function subtract() {
     return arguments[0] - arguments[1];
 }
 
-function multiply(){
+function multiply() {
     prod = 1;
-    for(let i = 0; i < arguments.length; i++){
+    for (let i = 0; i < arguments.length; i++) {
         prod = prod * arguments[i];
     }
 
     return prod;
 }
 
-function divide(){
+function divide() {
     return arguments[0] / arguments[1];
 }
 
-function operate(fun, a, b){
-    return fun(a,b);
+function operate(fun, a, b) {
+    return fun(a, b);
 };
 
